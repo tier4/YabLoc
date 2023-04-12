3d9e6f39107a5c7acaf9bb5e1084c38a6c89dc4a
Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>
2023-04-12 18:56:47 +0900

    add license description
    Signed-off-by: Kento Yabuuchi <kento.yabuuchi.2@tier4.jp>

---
+++ rviz2_plugins/rviz2_overlay_plugins/README.md
@@ -0,0 +1,3 @@
+# tier4_debug_rviz_plugin
+
+This package is including jsk code. Note that jsk_overlay_utils.cpp and jsk_overlay_utils.hpp are BSD license.
--- rviz2_plugins/rviz2_overlay_plugins/src/image_overlay_display.hpp
+++ rviz2_plugins/rviz2_overlay_plugins/src/image_overlay_display.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Tier IV, Inc.
+// Copyright 2023 Tier IV, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,23 +12,58 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Copyright (c) 2014, JSK Lab
+// All rights reserved.
+//
+// Software License Agreement (BSD License)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of {copyright_holder} nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.S SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef IMAGE_OVERLAY_DISPLAY_HPP_
 #define IMAGE_OVERLAY_DISPLAY_HPP_
 
+#include <rclcpp/qos.hpp>
+
 #include <memory>
 #include <mutex>
-#include <rclcpp/qos.hpp>
 
 #ifndef Q_MOC_RUN
 #include "jsk_overlay_utils.hpp"
 
 #include <image_transport/image_transport.hpp>
 #include <rviz_common/properties/color_property.hpp>
+#include <rviz_common/properties/editable_enum_property.hpp>
+#include <rviz_common/properties/enum_property.hpp>
 #include <rviz_common/properties/float_property.hpp>
 #include <rviz_common/properties/int_property.hpp>
-#include <rviz_common/properties/enum_property.hpp>
-#include <rviz_common/properties/editable_enum_property.hpp>
 #include <rviz_common/ros_topic_display.hpp>
+
 #include <image_transport/subscriber_filter.h>
 #include <message_filters/subscriber.h>
 #endif
